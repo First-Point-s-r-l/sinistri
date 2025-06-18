fb5702d60c31b681087482e9aae70866cbd06ce9
l-contini <l.contini@firstpoint.it>
2025-06-18 12:19:50 +0200

    Implementato logout

---
+++ Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+
+namespace AdvSinopia.Pages.Account
+{
+        public class LogoutModel : PageModel
+        {
+            public async Task<IActionResult> OnPostAsync()
+            {
+                await HttpContext.SignOutAsync();   // rimuove il cookie
+                return RedirectToPage("/Account/Login");
+            }
+        }
+}
